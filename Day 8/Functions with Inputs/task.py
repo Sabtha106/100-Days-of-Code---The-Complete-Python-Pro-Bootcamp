d9a3e8272dd3d2f6c3d255d816deb73d9fcb08e8
SabeloMab <sabtha106@gmail.com>
2026-01-22 16:34:47 +0200

    First commit of day-8 programming challenge

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+def greeting():
+    print("Hello")
+    print("how are you?")
+    print("how is the weather?")
+
+greeting()
+
+# functions that allows for inputs
+def greet_with_a_name(name):
+    print(f"Hello, {name}!")
+    print(f"How are you{name}?")
+
+
+greet_with_a_name("Sam")
